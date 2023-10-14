0e3d4422849f35dc3cfffb3e06bf89f0bd5942f4
AmanySami <amanysami_2012@yahoo.com>
2023-10-14 21:13:58 +0300

    Create N-Tier Architecture for Catalog.API

---
+++ src/Aspnet-Microservices/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        //GetProduct()-->list
+        Task<IEnumerable<Product>> GetProducts();
+        //GetProductById(string id)-->return one product based Id
+        Task<Product> GetProductById(string id);
+        //GetProductByName(string productName)---> return list from products based Product Name
+        Task<IEnumerable<Product>> GetProductByName(string productName);
+        //GetProductByCategoryName(string categoryName)---> return list from products based Category Name
+        Task<IEnumerable<Product>> GetProductByCategoryName(string categoryName);
+        //CreateProduct(Product productObject) -->Task
+        Task CreateProduct(Product product);
+        //UpdateProduct(Product product)-->return bool
+        Task<bool> UpdateProduct(Product product);
+        //DeleteProduct(string id)-->return bool
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
